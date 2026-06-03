c94e4816be5aa94ad5ad9e9a846a295bdd4f8f13
elis <elis95954092@gmail.com>
2026-06-02 21:50:45 -0300

    0206

---
+++ aula0206ex5/src/Tarefas.java
@@ -0,0 +1,14 @@
+public class Tarefas {
+    private  String  descricao;
+    private  boolean conluida;
+
+    public Tarefas(String d){
+        this.descricao = d;
+        this.conluida = false;
+    }
+    public void finalizarTarefa(){
+        this. conluida = true;
+    }
+
+}
+//ex2 terminat em casa
